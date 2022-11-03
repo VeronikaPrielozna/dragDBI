033fcd2799c19d49a2a7208f770a1e17c6d19d7b
VeronikaPrielozna <verca.prielozna@gmail.com>
2022-11-03 15:08:37 +0100

    3.11.

--- R/UniteData.R
+++ R/UniteData.R
@@ -13,17 +13,17 @@
 #' DBI_Data<-UniteData(Data, DBI_val = "CE")
 
 UniteData<-function(df, DBI_val, DBI_UD){
+  ms<-"This species is not in DBI dataset. Therefor this it will be removed."
+  miss<-vector()
   table_user<-df
   table_user[,1]<-gsub("[_]", " ", table_user[,1])
 
   if (DBI_val=="CE"){
     table_package<-DBI_CE
   }
-
   if(DBI_val=="SA"){
     table_package<-DBI_SA
   }
-
   if(DBI_val=="UD"){
     table_package<-DBI_UD
     table_spec<-table_package[,1]
@@ -31,15 +31,12 @@ UniteData<-function(df, DBI_val, DBI_UD){
     COLnum1<-0
     COLnum2<-2
   }
-
   if(DBI_val!="UD"){
     table_spec<-table_package$Species
     COLname<-c("Distribution", "Threat", "Sensitivity", "TOTAL")
     COLnum1<-3
     COLnum2<-4
   }
-
-
   for (j in 1:nrow(table_user)){
     if (substr(table_user[j,1], nchar(table_user[j,1]), nchar(table_user[j,1]))==" "){
       table_user[j,1]<-substring(table_user[j,1],1, nchar(table_user[j,1])-1)
@@ -71,16 +68,23 @@ UniteData<-function(df, DBI_val, DBI_UD){
         }
       }
       tabulka<-as.data.frame(as.matrix(vector_druhy))
+      tabulka<-rbind(tabulka, c("None!"))
       colnames(tabulka)<-c("Species")
       cat("\n")
       print(tabulka)
       cat("\n")
       answ=readline(prompt="Choosen: ")
-      table_user[j,1]<-tabulka[answ,]
+
+      if(tabulka[answ,]==c("None!")) {
+        miss<-c(miss,table_user[j,1])
+        table_user[j,1]<-"NA"
+      }
     }
+
     table_user[j,COLname]<-table_package[match(table_user[j,1], table_package[,1]), COLname]
+
+    #table_user<-table_user[, c(1,(ncol(table_user)-COLnum):ncol(table_user), 2:(ncol(table_user)-COLnum2))]
   }
-  table_user<-table_user[, c(1,(ncol(table_user)-COLnum1):ncol(table_user), 2:(ncol(table_user)-COLnum2))]
 
   if (DBI_val=="UD"){
     na_val<-which(table_user[,2]=="NA")
@@ -92,18 +96,26 @@ UniteData<-function(df, DBI_val, DBI_UD){
 
   if(length(na_val)>0){
     for (m in na_val){
-      cat(paste("DBI value of species", table_user[m, 1], "is NA, propably because of small amount of data."))
+      message(paste("DBI value of species", table_user[m, 1], "is NA, propably because of small amount of data."))
       cat("\n")
       message("Therefor this species will be removed.")
       table_user<-table_user[-(na_val),]
     }
   }
-  cat(" ")
   cat("\n")
-
   Species<-as.factor(table_user[,1])
   table_user<-as.data.frame(sapply(table_user[2:ncol(table_user)], as.numeric))
   table_user<-data.frame(Species, table_user)
+
+  if ("NA" %in% table_user$Species){
+    table_user<-table_user[-which(table_user[,1]=="NA"),]
+    cat(paste("Removed species:","\n"))
+    for (o in 1:length(miss)){
+      cat(paste("\t",miss[o],"\n"))
+    }
+  }
+  cat("\n")
+  table_user<-table_user[, c(1,(ncol(table_user)-COLnum1):ncol(table_user), 2:(ncol(table_user)-COLnum2))]
   print(table_user)
   table_user
 }
