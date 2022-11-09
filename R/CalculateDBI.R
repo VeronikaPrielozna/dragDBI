9a367c68eb24dc30253a9b7d6cf82e059c4224b3
VeronikaPrielozna <verca.prielozna@gmail.com>
2022-11-09 14:58:14 +0100

    9.11.

--- R/CalculateDBI.R
+++ R/CalculateDBI.R
@@ -95,6 +95,6 @@ CalculateDBI<-function(df, DBI_val, DBI_UD, NAval=F, sim=10000){
 
   table1<-round(table1,3)
   cat("Calculated set of DBI values","\n")
-  print(table1)
-  table1
+  print(as.data.frame(table1))
+  as.data.frame(table1)
 }
